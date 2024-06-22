08cb26b751688b4213f9367ac12350f4c3ed8daa
Dominik Fuka <fukd00@vse.cz>
2024-06-22 21:36:44 +0200

    admin muze smazat predmety

--- semestral-collectibles/controllers/adminController.js
+++ semestral-collectibles/controllers/adminController.js
@@ -1,6 +1,12 @@
-import { findAllItems } from "../models/itemModel.js";
+import { findAllItems, deleteItemById } from "../models/itemModel.js";
 
 export const showAllItems = async (req, res) => {
   const items = await findAllItems();
   res.render("admin/items", { items });
+};
+
+export const deleteAnyItem = async (req, res) => {
+  const { id } = req.params;
+  await deleteItemById(id);
+  res.redirect("/admin/items");
 };
--- semestral-collectibles/routes/adminRoutes.js
+++ semestral-collectibles/routes/adminRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { showAllItems } from "../controllers/adminController.js";
+import { showAllItems, deleteAnyItem } from "../controllers/adminController.js";
 import { requireAdmin } from "../middleware/admin.js";
 
 const router = express.Router();
 
 router.get("/admin/items", requireAdmin, showAllItems);
+router.post("/admin/items/delete/:id", requireAdmin, deleteAnyItem);
 
 export default router;
