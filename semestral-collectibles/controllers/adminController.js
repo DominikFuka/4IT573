6c5f98b92af7ca04ed85be296a67d792024ea2c3
Dominik Fuka <fukd00@vse.cz>
2024-06-25 19:50:42 +0200

    aktualizace seznamu predmetu u uzivatele kdyz predmet smaze admin

--- semestral-collectibles/controllers/adminController.js
+++ semestral-collectibles/controllers/adminController.js
@@ -1,4 +1,9 @@
-import { findAllItems, deleteItemById } from "../models/itemModel.js";
+import {
+  findAllItems,
+  deleteItemById,
+  findItemById,
+} from "../models/itemModel.js";
+import { broadcast } from "../server.js";
 
 /**
  * Display all items in the system (admin users only).
@@ -13,6 +18,11 @@ export const showAllItems = async (req, res) => {
  */
 export const deleteAnyItem = async (req, res) => {
   const { id } = req.params;
-  await deleteItemById(id);
+  const item = await findItemById(id);
+  // if item exists, delete item and send broadcast
+  if (item) {
+    await deleteItemById(id);
+    broadcast({ type: "DELETE_ITEM", itemId: id, ownerId: item.owner_id });
+  }
   res.redirect("/admin/items");
 };
